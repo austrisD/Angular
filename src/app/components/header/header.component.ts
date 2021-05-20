13e0fd2d3b32645532d73332b885882eb5495664
austrisD <austris.daugulis@gmail.com>
2021-05-21 00:31:33 +0300

    create project

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  title: string = 'tasks manager';
+  author: string = 'Austris'
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
